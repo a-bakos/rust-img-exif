1857bd48f237811d6ad90fe14b4ace367083b334
a-bakos <bakosattila@fastem.com>
2022-05-23 19:56:14 +0100

    Basic meta struct example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+pub mod image {
+    #[derive(Debug)]
+    pub struct Meta {
+        pub f_stop: f32,
+        pub iso: i32,
+        pub shutter: String,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let img: image::Meta = image::Meta {
+        f_stop: 22.0,
+        iso: 100,
+        shutter: String::from("1/500"),
+    };
+
+    println!("{:#?}", img);
 }
